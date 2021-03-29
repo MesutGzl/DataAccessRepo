4bd443f3c3ab694bf71b3434a5b6c9ec159d8a06
Mesut Güzel <71979276+MesutGzl@users.noreply.github.com>
2021-03-29 18:10:13 +0300

    Update BaseEntity.cs
    Açıklamalar eklendi

--- CodeFirst/Entities/Abstract/BaseEntity.cs
+++ CodeFirst/Entities/Abstract/BaseEntity.cs
@@ -4,14 +4,15 @@ namespace BasicDatabaseWithCodeFirst.Entities.Abstract
 {
     public abstract class BaseEntity
     {
+        //Her class'da sürekli olarak kullanacagim propertyleri BaseEntity classında kullanarak diğer classlara kalıtacağım.
         public int Id { get; set; }
 
         private DateTime _createDate = DateTime.Now;
         public DateTime CreateDate { get; set; }
         private DateTime _updateDate = DateTime.Now;
-        public DateTime? UpdateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }//Burada ? kullanmamın sebebi herhangi bir kayıdın oluşturulduğu zaman UpdateDate'i olmaz. Bu sebepten dolayı nullable olarak işaretledim.
 
-        public DateTime? DeleteDate { get; set; }
+        public DateTime? DeleteDate { get; set; }//Burada ? kullanmamın sebebi herhangi bir kayıdın oluşturulduğu zaman DeleteDate'i olmaz. Bu sebepten dolayı nullable olarak işaretledim.
 
     }
 }
