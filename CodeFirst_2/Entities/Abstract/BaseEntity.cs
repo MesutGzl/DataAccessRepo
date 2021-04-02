1f263a5bd86eb0feb0edafdd1d92659f945abaae
Mesut Güzel <71979276+MesutGzl@users.noreply.github.com>
2021-04-02 17:44:59 +0300

    Add files via upload

---
+++ CodeFirst_2/Entities/Abstract/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using CodeFirst_2.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst_2.Entities.Abstract
+{
+    public abstract class BaseEntity
+    {
+        public abstract int  Id { get; set; }
+        private DateTime _createDate = DateTime.Now; //Burada yeni eklenen ürünün kayıt tarihinin değiştirilmemesi için kapsülleme yaptık.
+
+        public DateTime CreateDate
+        {
+            get => _createDate;
+            set => _createDate = value;
+        }
+
+        public DateTime? UpdateDate { get; set; }   //Yeni oluşturulan product'un UpdateDate ve DeleteDate bilgileri olmaz. Bu sebepten dolayı "?" kullanarak nullable yani boş geçilebilir şekilde işaretledim.
+        public DateTime? DeleteDate { get; set; }
+
+        private Status _status = Status.Active;
+        public Status Status { get => _status; set => _status = value; }
+    }
+}
---
+++ CodeFirst_2/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿
+namespace CodeFirst_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCreateProductName = new System.Windows.Forms.TextBox();
+            this.txtCreateDescription = new System.Windows.Forms.TextBox();
+            this.txtCreateUnitPrice = new System.Windows.Forms.TextBox();
+            this.txtUpdateProductName = new System.Windows.Forms.TextBox();
+            this.txtUpdateDescription = new System.Windows.Forms.TextBox();
+            this.txtUpdateUnitPrice = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtDeleteProductID = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCreate);
+            this.groupBox1.Controls.Add(this.txtCreateUnitPrice);
+            this.groupBox1.Controls.Add(this.txtCreateDescription);
+            this.groupBox1.Controls.Add(this.txtCreateProductName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 275);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 163);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Create Product";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Product Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Description :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Unit Price :";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnUpdate);
+            this.groupBox2.Controls.Add(this.txtUpdateUnitPrice);
+            this.groupBox2.Controls.Add(this.txtUpdateDescription);
+            this.groupBox2.Controls.Add(this.txtUpdateProductName);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(257, 275);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(230, 163);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Update Product";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Unit Price :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Description :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Product Name :";
+            // 
+            // txtCreateProductName
+            // 
+            this.txtCreateProductName.Location = new System.Drawing.Point(93, 18);
+            this.txtCreateProductName.Name = "txtCreateProductName";
+            this.txtCreateProductName.Size = new System.Drawing.Size(100, 20);
+            this.txtCreateProductName.TabIndex = 1;
+            // 
+            // txtCreateDescription
+            // 
+            this.txtCreateDescription.Location = new System.Drawing.Point(93, 46);
+            this.txtCreateDescription.Name = "txtCreateDescription";
+            this.txtCreateDescription.Size = new System.Drawing.Size(100, 20);
+            this.txtCreateDescription.TabIndex = 1;
+            // 
+            // txtCreateUnitPrice
+            // 
+            this.txtCreateUnitPrice.Location = new System.Drawing.Point(93, 74);
+            this.txtCreateUnitPrice.Name = "txtCreateUnitPrice";
+            this.txtCreateUnitPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtCreateUnitPrice.TabIndex = 1;
+            // 
+            // txtUpdateProductName
+            // 
+            this.txtUpdateProductName.Location = new System.Drawing.Point(107, 18);
+            this.txtUpdateProductName.Name = "txtUpdateProductName";
+            this.txtUpdateProductName.Size = new System.Drawing.Size(100, 20);
+            this.txtUpdateProductName.TabIndex = 1;
+            // 
+            // txtUpdateDescription
+            // 
+            this.txtUpdateDescription.Location = new System.Drawing.Point(107, 46);
+            this.txtUpdateDescription.Name = "txtUpdateDescription";
+            this.txtUpdateDescription.Size = new System.Drawing.Size(100, 20);
+            this.txtUpdateDescription.TabIndex = 1;
+            // 
+            // txtUpdateUnitPrice
+            // 
+            this.txtUpdateUnitPrice.Location = new System.Drawing.Point(107, 74);
+            this.txtUpdateUnitPrice.Name = "txtUpdateUnitPrice";
+            this.txtUpdateUnitPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtUpdateUnitPrice.TabIndex = 1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnDelete);
+            this.groupBox3.Controls.Add(this.txtDeleteProductID);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Location = new System.Drawing.Point(503, 275);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(159, 98);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Delete Product";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Product ID :";
+            // 
+            // txtDeleteProductID
+            // 
+            this.txtDeleteProductID.Location = new System.Drawing.Point(76, 22);
+            this.txtDeleteProductID.Name = "txtDeleteProductID";
+            this.txtDeleteProductID.Size = new System.Drawing.Size(60, 20);
+            this.txtDeleteProductID.TabIndex = 1;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.dataGridView1);
+            this.groupBox4.Location = new System.Drawing.Point(12, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(776, 257);
+            this.groupBox4.TabIndex = 2;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "List Of Products";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(764, 232);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(76, 53);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(60, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(132, 116);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(118, 116);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtCreateUnitPrice;
+        private System.Windows.Forms.TextBox txtCreateDescription;
+        private System.Windows.Forms.TextBox txtCreateProductName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtUpdateUnitPrice;
+        private System.Windows.Forms.TextBox txtUpdateDescription;
+        private System.Windows.Forms.TextBox txtUpdateProductName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtDeleteProductID;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
